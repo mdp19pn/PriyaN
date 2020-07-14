f709221f39ca86f1662e762e514b39b478034778
mdp19pn <66477933+mdp19pn@users.noreply.github.com>
2020-07-14 16:41:41 +0100

    Update LuminalCellProperty.cpp

--- src/LuminalCellProperty.cpp
+++ src/LuminalCellProperty.cpp
@@ -1,1 +1,9 @@
+#include "LuminalCellProperty.hpp"
 
+LuminalCellProperty::LuminalCellProperty()
+    : AbstractCellMutationState(0)
+{}
+
+#include "SerializationExportWrapperForCpp.hpp"
+// Declare identifier for the serializer
+CHASTE_CLASS_EXPORT(LuminalCellProperty)
