118b2a67378b2b08986e02ef926dc54be9d86b08
Alex Fletcher <AlexFletcher@users.noreply.github.com>
2020-07-16 12:31:43 +0100

    Update CellVelocityWriter.cpp

--- src/CellVelocityWriter.cpp
+++ src/CellVelocityWriter.cpp
@@ -1,62 +1,64 @@
 #include "CellVelocityWriter.hpp"
 #include "AbstractCellPopulation.hpp"
 
+
+#include "CellVolumesWriter.hpp"
+#include "NodeBasedCellPopulation.hpp"
+#include "Exception.hpp"
+
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 CellVelocityWriter<ELEMENT_DIM, SPACE_DIM>::CellVelocityWriter()
-    : AbstractCellWriter<ELEMENT_DIM, SPACE_DIM>("cellvelocities.dat")
+    : AbstractCellWriter<ELEMENT_DIM, SPACE_DIM>("velocity.dat")
 {
-    this->mVtkCellDataName = "Velocity";
+    this->mVtkCellDataName = "Cell velocity";
 }
 
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 double CellVelocityWriter<ELEMENT_DIM, SPACE_DIM>::GetCellDataForVtkOutput(CellPtr pCell, AbstractCellPopulation<ELEMENT_DIM, SPACE_DIM>* pCellPopulation)
 {
-    // The method GetCellDataForVtkOutput() is not suitable for this class, so we simply return zero
-    return 0.0;
+    double volume = pCellPopulation->GetVolumeOfCell(pCell);
+    return volume;
 }
 
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void CellVelocityWriter<ELEMENT_DIM, SPACE_DIM>::VisitCell(CellPtr pCell, AbstractCellPopulation<ELEMENT_DIM, SPACE_DIM>* pCellPopulation)
 {
-    for (typename AbstractMesh<SPACE_DIM, SPACE_DIM>::NodeIterator node_iter = pCellPopulation->rGetMesh().GetNodeIteratorBegin();
-         node_iter != pCellPopulation->rGetMesh().GetNodeIteratorEnd();
-         ++node_iter)
+    if (dynamic_cast<NodeBasedCellPopulation<SPACE_DIM>*>(pCellPopulation))
     {
-        // We should never encounter deleted nodes when calling this method
+        unsigned node_index = pCellPopulation->GetLocationIndexUsingCell(pCell);
+        
+        // We should never encounter nodes that are deleted or associated with dead cells when calling this method
         assert(!node_iter->IsDeleted());
-
-        unsigned node_index = node_iter->GetIndex();
-
-        // Check that results should be written for this node
-        bool is_real_node = !(pCellPopulation->IsGhostNode(node_index));
-
-        // We should never encounter nodes associated with dead cells when calling this method
-        if (is_real_node)
+        if (!(pCellPopulation->IsGhostNode(node_index)))
         {
-            assert(!pCellPopulation->GetCellUsingLocationIndex(node_index)->IsDead());
+            assert(!pCell->IsDead());
+        }
+        
+        unsigned cell_id = pCell->GetCellId();
+        
+        
+        // Write the cell's ID to file
+        *this->mpOutStream << cell_id << " ";
+
+        // Write the cell's position to file
+        const c_vector<double, SPACE_DIM>& position = pCellPopulation->GetNode(node_index)->rGetLocation();
+        for (unsigned i=0; i<SPACE_DIM; i++)
+        {
+            *this->mpOutStream << position[i] << " ";
         }
 
-        if (is_real_node)
+        // Write the cell's velocity to file
+        double time_step = SimulationTime::Instance()->GetTimeStep();
+        double damping_constant = pCellPopulation->GetDampingConstant(node_index);
+        c_vector<double, SPACE_DIM> velocity = time_step * node_iter->rGetAppliedForce() / damping_constant;
+        for (unsigned i=0; i<SPACE_DIM; i++)
         {
-            // Write this node's index to file
-            *this->mpOutStream << node_index  << " ";
-
-            // Write this node's position to file
-            const c_vector<double, SPACE_DIM>& position = node_iter->rGetLocation();
-            for (unsigned i=0; i<SPACE_DIM; i++)
-            {
-                *this->mpOutStream << position[i] << " ";
-            }
-
-            // Write this node's velocity to file
-            double time_step = SimulationTime::Instance()->GetTimeStep(); ///\todo correct time step? (#2404)
-            double damping_constant = pCellPopulation->GetDampingConstant(node_index);
-            c_vector<double, SPACE_DIM> velocity = time_step * node_iter->rGetAppliedForce() / damping_constant;
-            for (unsigned i=0; i<SPACE_DIM; i++)
-            {
-                *this->mpOutStream << velocity[i] << " ";
-            }
-        }
+            *this->mpOutStream << velocity[i] << " ";
+        }      
+    }
+    else
+    {
+        EXCEPTION("This method is implemented only for a NodeBasedCellPopulation")
     }
 }
 
