b27b21e67b6550f69bace22e77ef1ca23b4caffe
mdp19pn <https://mdp19pn@github.com>
2020-07-24 16:04:46 +0000

    Add MammaryCellCycleModel Class

--- test/TestMammaryOrganoid.hpp
+++ test/TestMammaryOrganoid.hpp
@@ -38,46 +38,42 @@ public:
         nodes.push_back(new Node<2>(1,  false,  1.0, 0.0));
         NodesOnlyMesh<2> mesh;
         mesh.ConstructNodesWithoutMesh(nodes, 1.5);
-        
+
         // Create a vector of proliferative cells using the helper CellsGenerator
         std::vector<CellPtr> cells;
         CellsGenerator<MammaryCellCycleModel, 2> cells_generator;
         cells_generator.GenerateBasicRandom(cells, mesh.GetNumNodes());
-
+     
         // Use the mesh and cells to create a cell population
         NodeBasedCellPopulation<2> cell_population(mesh, cells);
-
+  
         // Create the luminal/myoepithelial cell properties (we do it this way
         // to make sure they're tracked correctly in the simulation)
         boost::shared_ptr<AbstractCellProperty> p_luminal(cell_population.GetCellPropertyRegistry()->Get<LuminalCellProperty>());
         boost::shared_ptr<AbstractCellProperty> p_myo(cell_population.GetCellPropertyRegistry()->Get<MyoepithelialCellProperty>());
-
+    
         // Assign these properties to cells (change these lines if you want e.g. only luminal cells)
         cell_population.GetCellUsingLocationIndex(0)->AddCellProperty(p_luminal);
         cell_population.GetCellUsingLocationIndex(1)->AddCellProperty(p_myo);
-
+   
         // Add a cell writer so that cell velocities are written to file
         cell_population.AddCellWriter<CellVelocityWriter>();
-
+       
         // Add a cell writer so that mammary cell types are written to file
         cell_population.AddCellWriter<MammaryCellTypeWriter>();
-
+       
         // Pass the cell population to the simulation and specify duration and output parameters
         OffLatticeSimulation<2> simulator(cell_population);
         simulator.SetOutputDirectory("TestMammaryMonolayer");
         simulator.SetSamplingTimestepMultiple(12);
         simulator.SetEndTime(96.0); // Hours
-
-        // Create a force law and pass it to the simulation
+       
+        // Create an adhesion force law and pass it to the simulation
         MAKE_PTR(CellCellAdhesionForce<2>, p_differential_adhesion_force);
         p_differential_adhesion_force->SetMeinekeSpringStiffness(50.0);
         p_differential_adhesion_force->SetHomotypicLabelledSpringConstantMultiplier(1.0);
         p_differential_adhesion_force->SetHeterotypicSpringConstantMultiplier(0.1);
         simulator.AddForce(p_differential_adhesion_force);
-
-        // Pass a force law for cell mechanics to the simulation
-        MAKE_PTR(GeneralisedLinearSpringForce<2>, p_force);
-        simulator.AddForce(p_force);
 
         // Run the simulation
         simulator.Solve();
@@ -131,10 +127,6 @@ public:
         p_differential_adhesion_force->SetHomotypicLabelledSpringConstantMultiplier(1.0);
         p_differential_adhesion_force->SetHeterotypicSpringConstantMultiplier(0.1);
         simulator.AddForce(p_differential_adhesion_force);
-
-        // Pass a force law for cell mechanics to the simulation
-        MAKE_PTR(GeneralisedLinearSpringForce<3>, p_force);
-        simulator.AddForce(p_force);
 
         // Run the simulation
         simulator.Solve();
