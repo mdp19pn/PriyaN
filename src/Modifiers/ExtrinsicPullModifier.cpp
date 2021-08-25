1fa7c5f5e424e13f962867afe181e3e4cd82f4ae
mdp19pn <pnarayanan1@sheffield.ac.uk>
2021-08-25 10:51:45 +0000

    Add Extrinsic Pull Modifier

--- src/Modifiers/ExtrinsicPullModifier.cpp
+++ src/Modifiers/ExtrinsicPullModifier.cpp
@@ -1,28 +1,20 @@
-#include "LumenExpansionForce.hpp"
+#include "ExtrinsicPullModifier.hpp"
 #include "NodeBasedCellPopulation.hpp"
 
 template<unsigned DIM>
-LumenExpansionForce<DIM>::LumenExpansionForce()
-    : AbstractForce<DIM>(),
-      mStrength(5.0)
+ExtrinsicPullModifier<DIM>::ExtrinsicPullModifier()
+    : AbstractCellBasedSimulationModifier<DIM>(),
+      mSpeed(1.0)
 {
 }
 
 template<unsigned DIM>
-double LumenExpansionForce<DIM>::GetStrength()
+ExtrinsicPullModifier<DIM>::~ExtrinsicPullModifier()
 {
-    return mStrength;
 }
 
 template<unsigned DIM>
-void LumenExpansionForce<DIM>::SetStrength(double strength)
-{
-    mStrength = strength;
-}
-
-
-template<unsigned DIM>
-void LumenExpansionForce<DIM>::AddForceContribution(AbstractCellPopulation<DIM>& rCellPopulation)
+void ExtrinsicPullModifier<DIM>::UpdateAtEndOfTimeStep(AbstractCellPopulation<DIM,DIM>& rCellPopulation)
 {
     // Define the initiall organoid lumen by centre and radius 
     c_vector<double,3> organoid_centre;
@@ -30,16 +22,16 @@ void LumenExpansionForce<DIM>::AddForceContribution(AbstractCellPopulation<DIM>&
     organoid_centre(1) = 0;
     organoid_centre(2) = 0;
 
+    unsigned num_nodes = rCellPopulation.GetNumNodes();
+    double dist_to_nb = 0.75;
+    double cell_radius = 0.5;
+    double lumen_radius = pow(num_nodes / 0.64, 1. / 3) * dist_to_nb / 2;
+
     // Iterate over cell population
     for (typename AbstractCellPopulation<DIM>::Iterator cell_iter = rCellPopulation.Begin();
         cell_iter != rCellPopulation.End();
         ++cell_iter)
     {
-        c_vector<double, 3> force = zero_vector<double>(3);
-        force(0) = -mStrength;
-        force(1) = -mStrength;
-        force(2) = -mStrength;
-
         // The increase of the lumen radius is proportional to the number of cells in each timestep. The cells are then pushed back.
         lumen_radius = pow(pow(lumen_radius,3)+num_nodes*pow(cell_radius,3), 1./3);
 
@@ -52,35 +44,44 @@ void LumenExpansionForce<DIM>::AddForceContribution(AbstractCellPopulation<DIM>&
             // If the location of the node falls inside the defined lumen region...
             if (previous_radius<lumen_radius)
             {
-                double strength = lumen_radius*(cell_location)/previous_radius;
+                double speed = lumen_radius*(cell_location)/previous_radius;
                 
                 unsigned node_index = rCellPopulation.GetLocationIndexUsingCell(*cell_iter);
                 
-                Node<DIM>* p_node = rCellPopulation.GetNode(node_index);
+                Node<3>* p_node = rCellPopulation.GetNode(node_index);
                 
                 // ...move the cell away from the lumen
-                p_node->rGetModifiableLocation() = strength;
+                p_node->rGetModifiableLocation() = speed;
             }
-
-            rCellPopulation.GetNode(node_index)->AddAppliedForceContribution(force);
         }
     }
 }
 
 template<unsigned DIM>
-void LumenExpansionForce<DIM>::OutputForceParameters(out_stream& rParamsFile)
+void ExtrinsicPullModifier<DIM>::SetupSolve(AbstractCellPopulation<DIM,DIM>& rCellPopulation, std::string outputDirectory)
 {
-    *rParamsFile << "\t\t\t<Strength>" << mStrength << "</Strength>\n";
-    
-    // Call method on direct parent class
-    AbstractForce<DIM>::OutputForceParameters(rParamsFile);
+}
+
+template<unsigned DIM>
+void ExtrinsicPullModifier<DIM>::SetSpeed(double speed)
+{
+    mSpeed = speed;
+}
+
+template<unsigned DIM>
+void ExtrinsicPullModifier<DIM>::OutputSimulationModifierParameters(out_stream& rParamsFile)
+{
+    *rParamsFile << "\t\t\t<Speed>" << mSpeed << "</Speed>\n";
+
+    // Next, call method on direct parent class
+    AbstractCellBasedSimulationModifier<DIM>::OutputSimulationModifierParameters(rParamsFile);
 }
 
 // Explicit instantiation
-template class LumenExpansionForce<1>;
-template class LumenExpansionForce<2>;
-template class LumenExpansionForce<3>;
+template class ExtrinsicPullModifier<1>;
+template class ExtrinsicPullModifier<2>;
+template class ExtrinsicPullModifier<3>;
 
 // Serialization for Boost >= 1.36
 #include "SerializationExportWrapperForCpp.hpp"
-EXPORT_TEMPLATE_CLASS_SAME_DIMS(LumenExpansionForce)
+EXPORT_TEMPLATE_CLASS_SAME_DIMS(ExtrinsicPullModifier)
